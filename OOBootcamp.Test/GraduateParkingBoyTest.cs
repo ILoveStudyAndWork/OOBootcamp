c93c752f089d7d77ad6f8ed335079c100e31b5ec
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-09-07 18:55:52 +0800

    [Fengyi] Fix test random failed issue

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -4,8 +4,14 @@ namespace OOBootcamp;
 
 public class GraduateParkingBoyTest
 {
-    private readonly GraduateParkingBoy _graduateParkingBoy = new GraduateParkingBoy();
-    
+    private GraduateParkingBoy _graduateParkingBoy;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _graduateParkingBoy = new GraduateParkingBoy();
+    }
+
     [Test]
     public void should_park_to_first_parking_lot_when_parking_given_parking_empty_and_no_car_park()
     {
@@ -17,10 +23,10 @@ public class GraduateParkingBoyTest
         var comingVehicle = new Vehicle("Eligible");
 
         string actualParkingLotName = _graduateParkingBoy.Parking(comingVehicle, (parkingLots));
-        
+
         Assert.AreEqual("first parkingLot", actualParkingLotName);
-    } 
-    
+    }
+
     [Test]
     public void should_park_to_second_parking_lot_when_parking_given_first_parking_lot_have_one_car_park_before()
     {
@@ -31,12 +37,10 @@ public class GraduateParkingBoyTest
         };
         var firstVehicle = new Vehicle("first");
         _graduateParkingBoy.Parking(firstVehicle, (parkingLots));
-        
+
         var secondVehicle = new Vehicle("second");
         var actualParkingName = _graduateParkingBoy.Parking(secondVehicle, (parkingLots));
-        
-        Assert.AreEqual("second parkingLot", actualParkingName);
-        
-    }
 
+        Assert.AreEqual("second parkingLot", actualParkingName);
+    }
 }
