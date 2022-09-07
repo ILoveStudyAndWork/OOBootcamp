694cd920a0491d86864f479dd45bfaa5ed0d8e16
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-09-07 18:25:33 +0800

    [Fengyi] Add second case

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -20,5 +20,23 @@ public class GraduateParkingBoyTest
         
         Assert.AreEqual("first parkingLot", actualParkingLotName);
     } 
+    
+    [Test]
+    public void should_park_to_second_parking_lot_when_parking_given_first_parking_lot_have_one_car_park_before()
+    {
+        ParkingLot[] parkingLots =
+        {
+            new(1, 6.0, "first parkingLot"),
+            new(1, 6.0, "second parkingLot")
+        };
+        var firstVehicle = new Vehicle("first");
+        _graduateParkingBoy.Parking(firstVehicle, (parkingLots));
+        
+        var secondVehicle = new Vehicle("second");
+        var actualParkingName = _graduateParkingBoy.Parking(secondVehicle, (parkingLots));
+        
+        Assert.AreEqual("second parkingLot", actualParkingName);
+        
+    }
 
 }
--- OOBootcamp/GraduateParkingBoy.cs
+++ OOBootcamp/GraduateParkingBoy.cs
@@ -2,8 +2,28 @@ namespace OOBootcamp;
 
 public class GraduateParkingBoy
 {
+    private List<string> parkingHistory = new List<string>();
+
     public string Parking(Vehicle comingVehicle, ParkingLot[] parkingLots)
     {
-        return parkingLots[0].Name;
+        if (parkingHistory.Count == 0)
+        {
+            parkingHistory.Add(parkingLots[0].Name);
+            return parkingLots[0].Name;
+        }
+
+        string lastParkingLotName = parkingHistory[^1];
+        int result = -1;
+        for (int index = 0; index < parkingLots.Length; index++)
+        {
+            if (parkingLots[index].Name.Equals(lastParkingLotName))
+            {
+                result = index;
+                break;
+            }
+        }
+
+        parkingHistory.Add(parkingLots[result + 1].Name);
+        return parkingLots[result + 1].Name;
     }
 }
