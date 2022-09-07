6c236e965d97701a59439649f569effe86657857
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-09-07 18:57:36 +0800

    [Fengyi] Introduce _parkingLots as shared field

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -6,6 +6,12 @@ public class GraduateParkingBoyTest
 {
     private GraduateParkingBoy _graduateParkingBoy;
 
+    private readonly ParkingLot[] _parkingLots =
+    {
+        new(1, 6.0, "first parkingLot"),
+        new(1, 6.0, "second parkingLot")
+    };
+
     [SetUp]
     public void SetUp()
     {
@@ -15,14 +21,9 @@ public class GraduateParkingBoyTest
     [Test]
     public void should_park_to_first_parking_lot_when_parking_given_parking_empty_and_no_car_park()
     {
-        ParkingLot[] parkingLots =
-        {
-            new(1, 6.0, "first parkingLot"),
-            new(1, 6.0, "second parkingLot")
-        };
         var comingVehicle = new Vehicle("Eligible");
 
-        string actualParkingLotName = _graduateParkingBoy.Parking(comingVehicle, (parkingLots));
+        string actualParkingLotName = _graduateParkingBoy.Parking(comingVehicle, _parkingLots);
 
         Assert.AreEqual("first parkingLot", actualParkingLotName);
     }
@@ -30,16 +31,11 @@ public class GraduateParkingBoyTest
     [Test]
     public void should_park_to_second_parking_lot_when_parking_given_first_parking_lot_have_one_car_park_before()
     {
-        ParkingLot[] parkingLots =
-        {
-            new(1, 6.0, "first parkingLot"),
-            new(1, 6.0, "second parkingLot")
-        };
         var firstVehicle = new Vehicle("first");
-        _graduateParkingBoy.Parking(firstVehicle, (parkingLots));
+        _graduateParkingBoy.Parking(firstVehicle, _parkingLots);
 
         var secondVehicle = new Vehicle("second");
-        var actualParkingName = _graduateParkingBoy.Parking(secondVehicle, (parkingLots));
+        var actualParkingName = _graduateParkingBoy.Parking(secondVehicle, _parkingLots);
 
         Assert.AreEqual("second parkingLot", actualParkingName);
     }
