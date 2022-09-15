e27a9c9995c2cd23245f13e5bfe8db919784c7e8
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-09-15 20:04:58 +0800

    [Fengyi] Remove mock and spy, Change Tasking.md

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Moq;
 using NUnit.Framework;
 
 namespace OOBootcamp;
@@ -9,17 +8,16 @@ public class GraduateParkingBoyTest
     private GraduateParkingBoy _graduateParkingBoy;
 
     private List<ParkingLot> parkingLots;
-    private Mock<ParkingLot> parkingLotA;
-    private Mock<ParkingLot> parkingLotB;
+    private ParkingLot parkingLotA;
+    private ParkingLot parkingLotB;
 
     private void SetUpWithDefault()
     {
-        parkingLotA = new Mock<ParkingLot>(2, 6, "a parking lot");
-        parkingLotB = new Mock<ParkingLot>(2, 6, "b parking lot");
+        parkingLotA = new ParkingLot(2, 6, "a parking lot");
+        parkingLotB = new ParkingLot(2, 6, "b parking lot");
         parkingLots = new List<ParkingLot>
         {
-            parkingLotA.Object,
-            parkingLotB.Object
+            parkingLotA, parkingLotB
         };
         _graduateParkingBoy = new GraduateParkingBoy(parkingLots);
     }
@@ -33,18 +31,16 @@ public class GraduateParkingBoyTest
         var actualParkingLot = _graduateParkingBoy.Parking(comingVehicle);
 
         Assert.AreEqual("a parking lot", actualParkingLot.Name);
-        parkingLotA.Verify(parkingLot => parkingLot.ParkVehicle(It.IsAny<Vehicle>()), Times.Once);
     }
 
     [Test]
     public void should_park_to_b_when_parking_given_a_not_available_and_no_past_parking()
     {
-        parkingLotA = new Mock<ParkingLot>(MockBehavior.Default, 0, 6, "a parking lot");
-        parkingLotB = new Mock<ParkingLot>(1, 6, "b parking lot");
+        parkingLotA = new ParkingLot(0, 6, "a parking lot");
+        parkingLotB = new ParkingLot(1, 6, "b parking lot");
         parkingLots = new List<ParkingLot>
         {
-            parkingLotA.Object,
-            parkingLotB.Object
+            parkingLotA, parkingLotB
         };
         _graduateParkingBoy = new GraduateParkingBoy(parkingLots);
         var comingVehicle = new Vehicle("Coming");
@@ -52,7 +48,6 @@ public class GraduateParkingBoyTest
         var actualParkingLot = _graduateParkingBoy.Parking(comingVehicle);
 
         Assert.AreEqual("b parking lot", actualParkingLot.Name);
-        parkingLotB.Verify(parkingLot => parkingLot.ParkVehicle(It.IsAny<Vehicle>()), Times.Once);
     }
 
     [Test]
@@ -66,8 +61,6 @@ public class GraduateParkingBoyTest
         var actualParkingLot = _graduateParkingBoy.Parking(comingVehicle);
 
         Assert.AreEqual("b parking lot", actualParkingLot.Name);
-        parkingLotA.Verify(parkingLot => parkingLot.ParkVehicle(pastVehicle), Times.Once);
-        parkingLotB.Verify(parkingLot => parkingLot.ParkVehicle(comingVehicle), Times.Once);
     }
 
     [Test]
@@ -83,20 +76,17 @@ public class GraduateParkingBoyTest
         var actualParkingLot = _graduateParkingBoy.Parking(comingVehicle);
 
         Assert.AreEqual("a parking lot", actualParkingLot.Name);
-        parkingLotA.Verify(parkingLot => parkingLot.ParkVehicle(comingVehicle), Times.Once());
     }
 
     [Test]
     public void should_park_to_b_when_parking_given_a_not_available_and_b_available_and_have_past_parking_to_a()
     {
-        var parkingLotOriginalA = new ParkingLot(1, 6, "a parking lot");
-        var parkingLotOriginalB = new ParkingLot(2, 6, "b parking lot");
-        parkingLotA = new Mock<ParkingLot>(parkingLotOriginalA){CallBase = true};
-        parkingLotB = new Mock<ParkingLot>(parkingLotOriginalB){CallBase = true};
+        parkingLotA = new ParkingLot(1, 6, "a parking lot");
+        parkingLotB = new ParkingLot(2, 6, "b parking lot");
         parkingLots = new List<ParkingLot>
         {
-            parkingLotOriginalA,
-            parkingLotOriginalB
+            parkingLotA,
+            parkingLotB
         };
         _graduateParkingBoy = new GraduateParkingBoy(parkingLots);
         var lastPastVehicle = new Vehicle("last park");
@@ -111,14 +101,11 @@ public class GraduateParkingBoyTest
     [Test]
     public void should_park_to_b_when_parking_given_a_not_available_and_b_available_and_have_past_parking_to_b()
     {
-        var parkingLotOriginalA = new ParkingLot(1, 6, "a parking lot");
-        var parkingLotOriginalB = new ParkingLot(2, 6, "b parking lot");
-        parkingLotA = new Mock<ParkingLot>(parkingLotOriginalA){CallBase = true};
-        parkingLotB = new Mock<ParkingLot>(parkingLotOriginalB){CallBase = true};
+        parkingLotA = new ParkingLot(1, 6, "a parking lot");
+        parkingLotB = new ParkingLot(2, 6, "b parking lot");
         parkingLots = new List<ParkingLot>
         {
-            parkingLotOriginalA,
-            parkingLotOriginalB
+            parkingLotA, parkingLotB
         };
         _graduateParkingBoy = new GraduateParkingBoy(parkingLots);
         var secondLastVehicle = new Vehicle("second last park");
--- Tasking.md
+++ Tasking.md
@@ -1,14 +1,14 @@
 ## Park Vehicle
 
-- when 1 vehicle coming, given a and b all available and no past parking, then parking to a
-- when 1 vehicle coming, given only a not available and no past parking, then parking to b
-- when 1 vehicle coming, given a and b all available and have past parking to a, then parking to b. 
-- when 1 vehicle coming, give a and b all available and have past parking to b, then parking to a 
-- when 1 vehicle coming, give a is full and b available and past parking to a, then parking to b 
-- when 1 vehicle coming, give a is full and b available and past parking to b, then parking to b 
-- when 1 vehicle coming, give a available and b is full and past parking to a, then parking to a 
-- when 1 vehicle coming, give a available and b is full and past parking to b, then parking to b 
-- when 1 vehicle coming, given a and b all full, and throw exception
+1. when 1 vehicle coming, given a and b all available and no past parking, then parking to a
+2. when 1 vehicle coming, given only a not available and no past parking, then parking to b
+3. when 1 vehicle coming, given a and b all available and have past parking to a, then parking to b. 
+4. when 1 vehicle coming, give a and b all available and have past parking to b, then parking to a 
+5. when 1 vehicle coming, give a is full and b available and past parking to a, then parking to b 
+6. when 1 vehicle coming, give a is full and b available and past parking to b, then parking to b 
+7. when 1 vehicle coming, give a available and b is full and past parking to a, then parking to a 
+8. when 1 vehicle coming, give a available and b is full and past parking to b, then parking to b 
+9. when 1 vehicle coming, given a and b all full, and throw exception
 
 Optional: multiple parkingLots(more than 2)
 
